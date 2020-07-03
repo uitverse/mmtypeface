f42ea49e7eb72fbf3536c05c5e99320b92718e6b
Frenco <frenco.willian@gmail.com>
2020-07-03 11:39:19 +0630

    removed file reading part from api

--- pages/api/[font].ts
+++ pages/api/[font].ts
@@ -1,4 +1,4 @@
-import { promises } from 'fs'
+// import { promises } from 'fs'
 
 import { NextApiRequest, NextApiResponse } from 'next'
 
@@ -9,12 +9,13 @@ export default async (
   const {
     query: { font },
   } = req
-  const { readFile } = promises
+//   const { readFile } = promises
 
-  const fonts: Array<string> = JSON.parse(await readFile('list.json', 'utf-8'))
+//   const fonts: Array<string> = JSON.parse(await readFile('list.json', 'utf-8'))
+  const fonts: Array<string> = ['padauk']
 
   if (fonts.includes(font as string)) {
-    const css = await readFile(`fonts/${font}.css`, 'utf-8')
+//     const css = await readFile(`fonts/${font}.css`, 'utf-8')
     res.end(css)
   } else {
     res.end(`Font not Found! ${font}`)
