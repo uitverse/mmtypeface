441e37159f9110c7de855d0241e3d653c8a7163c
Frenco <hey@frenco.dev>
2020-09-13 21:52:36 +0630

    feat: make font interface to have urls conditionally

--- index.d.ts
+++ index.d.ts
@@ -1,6 +1,6 @@
 declare module 'fonts.yaml' {
   import type { FontFamily } from '@lib/decode.interface'
 
-  const fonts: Array<FontFamily>
+  const fonts: Array<FontFamily<0>>
   export default fonts
 }
--- lib/decode.interface.ts
+++ lib/decode.interface.ts
@@ -1,13 +1,11 @@
-export interface FontFamily {
+export interface FontFamily<T extends unknown = undefined> {
   family: string
-  fonts: Array<Font>
+  fonts: Array<Font<T>>
 }
 
-export interface Font {
-  weight: string
-  style: string
-  urls?: Array<FontUrl>
-}
+export type Font<T extends unknown = undefined> = T extends undefined
+  ? { weight: string; style: string }
+  : { weight: string; style: string; urls: Array<FontUrl> }
 
 export interface FontUrl {
   format: 'woff' | 'woff2'
