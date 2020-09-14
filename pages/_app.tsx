07f8887e94b1ceccaaf94a84004832e3ce425131
Frenco <hey@frenco.dev>
2020-09-14 19:37:51 +0630

    feat: add custom _document

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,6 +2,6 @@ import '@styles/tailwind.css'
 
 import { AppProps } from 'next/app'
 
-export default function App({ Component, pageProps }: AppProps) {
+export default function MyApp({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />
 }
