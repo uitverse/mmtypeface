02df3863172b02672a85649a238777d568b41c18
Frenco <hey@frenco.dev>
2020-09-14 12:43:19 +0630

    feat: add 24hr caching for api with lru

--- pages/api/index.tsx
+++ pages/api/index.tsx
@@ -3,13 +3,34 @@ import { Display, isDisplay } from '@lib/display'
 import search from '@lib/search'
 import transform from '@lib/transform'
 import data from 'fonts.yaml'
+import LRUCache from 'lru-cache'
 import { NextApiRequest, NextApiResponse } from 'next'
 
-export default function API(req: NextApiRequest, res: NextApiResponse): void {
-  const families = req.query.family ? decode(req.query.family) : []
-  const display: Display = isDisplay(req.query.display)
-    ? req.query.display
-    : 'swap'
+const cache = new LRUCache({
+  max: 100,
+  maxAge: 1000 * 60 * 60 * 24,
+})
 
-  res.status(200).end(transform(Array.from(search(data, families)), display))
+export default function API(req: NextApiRequest, res: NextApiResponse): void {
+  const key = req.url
+
+  if (cache.has(key)) {
+    res.setHeader('x-cache', 'HIT')
+    res.status(200).send(cache.get(key))
+  } else {
+    try {
+      const families = req.query.family ? decode(req.query.family) : []
+      const display: Display = isDisplay(req.query.display)
+        ? req.query.display
+        : 'swap'
+      const result = transform(Array.from(search(data, families)), display)
+
+      cache.set(key, result)
+      res.setHeader('x-cache', 'MISS')
+      res.status(200).send(result)
+    } catch (e) {
+      res.setHeader('x-cache', 'SKIP')
+      res.status(502).end()
+    }
+  }
 }
