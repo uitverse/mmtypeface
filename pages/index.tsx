46799b92eacd42503c37632202ef073bd4d569d1
Frenco <hey@frenco.dev>
2020-09-17 09:08:00 +0630

    feat: remove fake data generator

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,16 +1,9 @@
 import Nav from '@components/nav'
 import { PREVIEWS } from '@lib/constants'
-import type { FontFamily } from '@lib/decode.interface'
 import data from 'fonts.yaml'
 import Head from 'next/head'
 import Link from 'next/link'
 import { useState } from 'react'
-
-function* makeData(): Generator<FontFamily<0>> {
-  for (let i = 0; i < 11; i++) {
-    yield data[0]
-  }
-}
 
 export default function HomePage(): JSX.Element {
   const [preview, ,] = useState(PREVIEWS.SENTENCE)
@@ -18,17 +11,20 @@ export default function HomePage(): JSX.Element {
   return (
     <>
       <Head>
-        <link
-          rel="stylesheet"
-          type="text/css"
-          href="https://fonts.googleapis.com/css2?family=Padauk&display=swap"
-        />
+        {data.map(({ family }) => (
+          <link
+            key={family}
+            rel="stylesheet"
+            type="text/css"
+            href={`/api?family=${family.replace(/\W/g, '+')}`}
+          />
+        ))}
       </Head>
       <main className="font-san shadow-border-top-brand">
         <Nav index={0} />
         <div className="w-full p-4 md:pt-3 md:px-0 md:mx-auto md:max-w-4xl">
           <div className="grid grid-flow-row gap-3 md:grid-cols-3">
-            {Array.from(makeData()).map(({ family, author, fonts }) => (
+            {data.map(({ family, author, fonts }) => (
               <Link
                 href={`/specimen/${family.replace(/\W/g, '+')}`}
                 key={family}>
