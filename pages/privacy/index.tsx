ec8e32300ae76a47f7e8928a89f157945d7b3977
Frenco <hey@frenco.dev>
2020-10-16 22:18:20 +0630

    feat: add legally required pages

--- pages/privacy/index.tsx
+++ pages/privacy/index.tsx
@@ -1,11 +1,11 @@
 import Nav from '@components/nav'
 
-export default function AboutPage(): JSX.Element {
+export default function PrivacyPage(): JSX.Element {
   return (
     <main>
       <Nav index={1} />
       <div className="w-full p-4 md:pt-3 md:mx-auto md:max-w-4xl lg:max-w-screen-xl">
-        About Page
+        Privacy &amp; Policy
       </div>
     </main>
   )
--- pages/terms/index.tsx
+++ pages/terms/index.tsx
@@ -1,11 +1,11 @@
 import Nav from '@components/nav'
 
-export default function AboutPage(): JSX.Element {
+export default function TermsPage(): JSX.Element {
   return (
     <main>
       <Nav index={1} />
       <div className="w-full p-4 md:pt-3 md:mx-auto md:max-w-4xl lg:max-w-screen-xl">
-        About Page
+        Terms of Services
       </div>
     </main>
   )
